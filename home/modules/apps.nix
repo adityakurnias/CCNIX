5d948c1fe4837eb557cf6068417c3c1279e80a71
adityakurnias <adityakurniasaputra903@gmail.com>
2025-10-13 18:19:36 +0700

    Fix: Correctly format XDG_DATA_DIRS as string, set browser to zen (idk if it's working)

--- home/modules/apps.nix
+++ home/modules/apps.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
     # Internet
-    firefox
+    # firefox
     thunderbird
     vesktop
 
--- home/modules/xdg.nix
+++ home/modules/xdg.nix
@@ -1,16 +1,32 @@
-{ pkgs, ... }:
+# Pastikan 'lib' ada di sini
+{ pkgs, config, lib, ... }:
 
 {
   xdg.mime.enable = true;
-
   xdg.mimeApps.defaultApplications = {
+    "text/html"              = "app.zen_browser.zen.desktop";
     "x-scheme-handler/http"  = "app.zen_browser.zen.desktop";
     "x-scheme-handler/https" = "app.zen_browser.zen.desktop";
-    "text/html"              = "app.zen_browser.zen.desktop";
   };
-  
+
+  dconf.settings = {
+    "org/gnome/desktop/default-applications/web" = {
+      "browser" = "app.zen_browser.zen.desktop";
+    };
+  };
+
   home.sessionVariables = {
     BROWSER = "flatpak run app.zen_browser.zen";
     DEFAULT_BROWSER = "flatpak run app.zen_browser.zen";
+    # Gunakan fungsi ini untuk mengubah list menjadi string
+    XDG_DATA_DIRS = lib.strings.concatStringsSep ":" [
+      # Menambahkan path Flatpak ke depan
+      "${config.home.homeDirectory}/.local/share/flatpak/exports/share"
+      "/var/lib/flatpak/exports/share"
+      # Path default sistem
+      "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}"
+      "${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}"
+      "/run/current-system/sw/share"
+    ];
   };
 }
