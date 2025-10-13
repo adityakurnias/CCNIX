a9ac3207718a866b3be863cce389dba36c05e827
adityakurnias <adityakurniasaputra903@gmail.com>
2025-10-13 18:21:44 +0700

    Fix: Correctly format XDG_DATA_DIRS as string, set browser to zen (idk if it's working)

--- home/modules/xdg.nix
+++ home/modules/xdg.nix
@@ -19,14 +19,15 @@
     BROWSER = "flatpak run app.zen_browser.zen";
     DEFAULT_BROWSER = "flatpak run app.zen_browser.zen";
     # Gunakan fungsi ini untuk mengubah list menjadi string
-    XDG_DATA_DIRS = lib.strings.concatStringsSep ":" [
-      # Menambahkan path Flatpak ke depan
-      "${config.home.homeDirectory}/.local/share/flatpak/exports/share"
-      "/var/lib/flatpak/exports/share"
-      # Path default sistem
-      "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}"
-      "${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}"
-      "/run/current-system/sw/share"
-    ];
+    # XDG_DATA_DIRS = lib.strings.concatStringsSep ":" [
+    #   # Menambahkan path Flatpak ke depan
+    #   "${config.home.homeDirectory}/.local/share/flatpak/exports/share"
+    #   "/var/lib/flatpak/exports/share"
+    #   # Path default sistem
+    #   "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}"
+    #   "${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}"
+    #   "/run/current-system/sw/share"
+    # ];
+    XDG_DATA_DIRS = "/var/lib/flatpak/exports/share";
   };
 }
