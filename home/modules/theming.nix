7c2aa24e3e791a2ae730844e5d2ff494f31e5f5e
adityakurnias <adityakurniasaputra903@gmail.com>
2025-10-26 11:56:55 +0700

    Update pkgs and add fastfetch config to repository

--- home/modules/theming.nix
+++ home/modules/theming.nix
@@ -1,38 +1,11 @@
 { pkgs, ... }:
-let
-  tetoCursor = builtins.path {
-    path = ../themes/TetoCursor;
-    name = "TetoCursor";
-    recursive = true;
-  };
-in
+
 {
   home.packages = with pkgs; [
     qt6ct
     bibata-cursors
   ];
-
-  home.pointerCursor = {
-    name = "TetoCursor";
-    package = pkgs.buildEnv {
-      name = "teto-cursor-theme";
-      paths = [
-        (pkgs.runCommand "teto-cursor-theme" { } ''
-          mkdir -p $out/share/icons
-          cp -r ${tetoCursor} $out/share/icons/TetoCursor
-        '')
-      ];
-    };
-    size = 24;
-  };
-
-  gtk.cursorTheme.name = "TetoCursor";
-  gtk.cursorTheme.size = 24;
-  xsession.pointerCursor = {
-    name = "TetoCursor";
-    size = 24;
-  };
-
+  
   home.sessionVariables = {
     QT_QPA_PLATFORMTHEME = "qt6ct";
   };
