a5172b5728268ad4fe1e7e41367415f2ee0b908b
adityakurnias <adityakurniasaputra903@gmail.com>
2026-01-05 19:49:59 +0700

    update

--- home/modules/apps/internet.nix
+++ home/modules/apps/internet.nix
@@ -2,7 +2,7 @@
 
 {
   home.packages = with pkgs; [
-    vesktop
+    # vesktop
     discord
     google-chrome
   ];
--- modules/services.nix
+++ modules/services.nix
@@ -4,4 +4,5 @@
   services.gnome.gnome-keyring.enable = true;
   services.flatpak.enable = true;
   services.printing.enable = false;
+  services.gvfs.enable = true;
 }
