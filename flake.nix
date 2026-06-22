683103fa28b2039c393368edfe940ff63793df04
adityakurnias <adityakurniasaputra903@gmail.com>
2026-06-22 17:50:14 +0700

    Update driver

--- flake.nix
+++ flake.nix
@@ -10,7 +10,8 @@
     };
 
     noctalia = {
-      url = "github:noctalia-dev/noctalia-shell/v5";
+      url = "github:noctalia-dev/noctalia-shell";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     niri = {
