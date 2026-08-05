2b6e7ae90f16df0812c1eddf3c234769ee4ecc0d
adityakurnias <adityakurniasaputra903@gmail.com>
2026-08-05 21:34:39 +0700

    hermes agent via nixos module, update xdg portal and deps

--- hosts/ccnixos/configuration.nix
+++ hosts/ccnixos/configuration.nix
@@ -10,7 +10,7 @@
     ./hardware-configuration.nix
     ../../fw/firmware.nix
     ../../profiles/core
-    ../../profiles/kurnias/hermes-agent
+    ../../profiles/kurnias/hermes-agent.nix
   ];
 
   fonts.fontDir.enable = true;
